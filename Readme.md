25416f4b71c97b9372b056f39e22ef6d0aa5a3e1
Alin Jurj <office@perfectweb.ro>
2019-02-18 11:10:33 +0200

    add 2nd test for check

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-#requirements
+# requirements
 * docker
 * docker-compose
 
@@ -9,5 +9,5 @@ run `./run.sh`
 running the entrypoint of the test suite thru `./run.sh` will:
 * create and build containers
 * start vhost containers that contain ssh server
-* start test containers for each of the php, node.js and python implementation
+* start test containers for each of the php, node.js and python implementation; each test will run 2 times on the same dataset
 
--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ echo -e '>> preparing code'
 docker-compose exec php ./composer.phar install
 echo -e '\n>> running tests'
 docker-compose exec php php test.php /instances.yaml
+echo -e '\n>> running 2nd test'
+docker-compose exec php php test.php /instances.yaml
 
 echo -e '\n> Running node.js implementation of Qlearsite'
 echo -e '>> preparing code'
@@ -23,12 +25,16 @@ prepareContainers &>/dev/null
 docker-compose exec node npm install
 echo -e '\n>> running tests'
 docker-compose exec node node test.js
+echo -e '\n>> running 2nd test'
+docker-compose exec node node test.js
 
 echo -e '\n> Running python implementation of Qlearsite'
 echo -e '>> preparing code'
 prepareContainers &>/dev/null
 docker-compose exec python pip install pyyaml cryptography==2.4.2 paramiko
 echo -e '\n>> running tests'
+docker-compose exec python python test.py /instances.yaml
+echo -e '\n>> running 2nd test'
 docker-compose exec python python test.py /instances.yaml
 
 echo -e '\n> cleaning up'
